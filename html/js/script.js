ed85458a5b2a35ca1d0bf49e5ef3d84f08d9b553
Will Dev <rafaelmelo75831@gmail.com>
2024-07-29 23:01:24 -0300

    adding function

--- html/js/script.js
+++ html/js/script.js
@@ -2,6 +2,11 @@ const mario = document.querySelector('.mario');
 
 const jump = () => {
     mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump'); //Quando a animacao acontecer, vai remover */
+    }, 500);
+
 }
 
 
